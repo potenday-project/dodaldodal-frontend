cbb2e4615ffa59501121cf65d796402ef95f8cf7
HyungWookChoi <qpflapffhs76@gmail.com>
2023-12-10 17:42:00 +0900

    set-up folder structure

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindConfig: './tailwind.config.ts',
+}
