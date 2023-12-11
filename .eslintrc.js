9804bd8db21019930c9b5ffa5e7ec6e25ff70619
HyungWookChoi <qpflapffhs76@gmail.com>
2023-12-11 10:35:55 +0900

    🔧 set-up lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,69 @@
 module.exports = {
-  extends: 'next/core-web-vitals',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:import/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'import', '@tanstack/query'],
+  rules: {
+    curly: ['error', 'all'],
+    eqeqeq: [
+      'error',
+      'always',
+      {
+        null: 'ignore',
+      },
+    ],
+    'no-warning-comments': [
+      'warn',
+      {
+        terms: ['todo', 'fixme', 'xxx'],
+        location: 'anywhere',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        pathGroups: [
+          {
+            pattern: 'next/**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@/components/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
 }
