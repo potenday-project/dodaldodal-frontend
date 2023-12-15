0a113567acd8a70d65eb2cdf876c16af0dbe4cdd
HyungWookChoi <qpflapffhs76@gmail.com>
2023-12-15 17:26:48 +0900

    🔒️ add rewrites for hide SERVER URL

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/:path*`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
