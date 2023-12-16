4941b50dbf51a32e53efc17866eff4bf9a5bf816
HyungWookChoi <qpflapffhs76@gmail.com>
2023-12-16 21:08:08 +0900

    ✨ add metadata

--- app/(route)/layout.tsx
+++ app/(route)/layout.tsx
@@ -15,6 +15,21 @@ const myFont = localFont({
 export const metadata: Metadata = {
   title: '도달도달 - 1:1 매칭 목표 달성 서비스',
   description: '1:1 매칭 목표 달성 서비스',
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    url: 'https://dodaldodal-frontend.vercel.app/',
+    title: '도달도달 - 1:1 매칭 목표 달성 서비스',
+    description: '1:1 매칭 목표 달성 서비스',
+    images: [
+      {
+        url: 'https://dodals3.s3.ap-northeast-2.amazonaws.com/asset/dodaldodal_rectangle.png',
+        width: 800,
+        height: 600,
+        alt: '도달도달',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
