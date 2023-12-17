d9e2ac041c35adf4e28e77ef3e2bf6d2c0047ab4
HyungWookChoi <qpflapffhs76@gmail.com>
2023-12-17 09:19:39 +0900

    ✨ add home redirect

--- app/(route)/(index)/layout.tsx
+++ app/(route)/(index)/layout.tsx
@@ -1,15 +1,18 @@
+import { UserContextProvider } from '@/app/_components/providers/UserProvider'
 import BottomNavigation from '@/app/_components/shared/bottom-navigation'
 import Header from '@/app/_components/shared/header'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className='h-full bg-home-background bg-cover bg-center bg-no-repeat'>
-      <Header>
-        <div />
-        <Header.AlarmIcon />
-      </Header>
-      {children}
-      <BottomNavigation selected='home' />
-    </main>
+    <UserContextProvider>
+      <main className='h-full bg-home-background bg-cover bg-center bg-no-repeat'>
+        <Header>
+          <div />
+          <Header.AlarmIcon />
+        </Header>
+        {children}
+        <BottomNavigation selected='home' />
+      </main>
+    </UserContextProvider>
   )
 }
